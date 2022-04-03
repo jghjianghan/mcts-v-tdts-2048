d4a6cd3be1d400f81d00db0fdd1aaa084eb953bb
Jiang Han <jianghan99@gmail.com>
2022-04-03 14:51:15 +0700

    Create Logger class, pending config printing'

--- src/controller/Experimentor.java
+++ src/controller/Experimentor.java
@@ -6,69 +6,33 @@ import agent.mcts.MctsAgent;
 import game.GameAction;
 import game.GameModel;
 import game.GameModel.GameState;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import io.ExperimentLogger;
 
 /**
  *
  * @author Jiang Han
  */
 public class Experimentor {
+
     public static void main(String[] args) {
         GameModel infModel = new GameModel(Integer.MAX_VALUE);
-        
         GamePlayingAgent agent = new MctsAgent();
         GameState state = infModel.generateInitialState();
-        
-        String directoryName = "log";
-        String fileName = "mcts-" + System.currentTimeMillis() + ".txt";
-        
-        Path dirPath = Paths.get(directoryName);
-        Path filePath = Paths.get(directoryName, fileName);
-        
-        File directory = dirPath.toFile();
-        if (!directory.exists())
-        {
-            directory.mkdir();
-        }
-        
-        try {
-            Files.write(
-                    filePath, 
-                    (state + "\n").getBytes(), 
-                    StandardOpenOption.CREATE);
-        } catch (IOException ex) {
-            Logger.getLogger(Experimentor.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        try {
-            do {
-                GameState copyState = state.copy();
-                long startTime = System.currentTimeMillis();
-                GameAction chosenAction = agent.selectAction(copyState, new GameModel(1000000));
-                long endTime = System.currentTimeMillis();
-                System.out.println("Duration: " + (endTime - startTime) + " ms");
-                Files.write(
-                        filePath, 
-                        ("Action: " + chosenAction + "\n").getBytes(), 
-                        StandardOpenOption.APPEND);
-                state = infModel.applyAction(state, chosenAction);
-                Files.write(
-                        filePath, 
-                        (state + "\n").getBytes(), 
-                        StandardOpenOption.APPEND);
-                
-                System.out.println("Score: " + state.getScore());
-            } while (!state.isTerminal());
-        } catch (IOException ex){
-            Logger.getLogger(Experimentor.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        ExperimentLogger logger = new ExperimentLogger();
+        logger.log(state);
+
+        do {
+            GameState copyState = state.copy();
+            long startTime = System.currentTimeMillis();
+            GameAction chosenAction = agent.selectAction(copyState, new GameModel(1000000));
+            long endTime = System.currentTimeMillis();
+
+            state = infModel.applyAction(state, chosenAction);
+            System.out.println("Duration: " + (endTime - startTime) + " ms");
+            logger.log(chosenAction, state);
+
+            System.out.println("Score: " + state.getScore());
+        } while (!state.isTerminal());
+
     }
 }
---
+++ src/io/ExperimentLogger.java
@@ -0,0 +1,65 @@
+package io;
+
+import controller.Experimentor;
+import game.GameAction;
+import game.GameModel.GameState;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jiang Han
+ */
+public class ExperimentLogger {
+    Path filePath;
+    
+    public static void main(String[] args) {
+    }
+    
+    public ExperimentLogger() {
+        String directoryName = "log";
+        
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+        String fileName = "mcts-" + now.format(formatter) + ".txt";
+        
+        Path dirPath = Paths.get(directoryName);
+        filePath = Paths.get(directoryName, fileName);
+        
+        File directory = dirPath.toFile();
+        if (!directory.exists())
+        {
+            directory.mkdir();
+        }
+    }
+    
+    public void log(GameState state){
+        try {
+            Files.write(
+                    filePath, 
+                    (state + "\n").getBytes(), 
+                    StandardOpenOption.CREATE);
+        } catch (IOException ex) {
+            Logger.getLogger(Experimentor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void log(GameAction action, GameState state){
+        try {
+            Files.write(
+                    filePath, 
+                    ("Action: " + action + "\n" + state + "\n").getBytes(), 
+                    StandardOpenOption.CREATE);
+        } catch (IOException ex) {
+            Logger.getLogger(Experimentor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
