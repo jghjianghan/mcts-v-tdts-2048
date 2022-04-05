a2dd70bc21d91f39011ce4d57bfbb7dc3d23f1ab
Jiang Han <jianghan99@gmail.com>
2022-04-05 16:12:40 +0700

    Emptied unused MctsAgentTest class

--- test/MctsAgentTest.java
+++ test/MctsAgentTest.java
@@ -12,39 +12,8 @@ import org.junit.Test;
  */
 public class MctsAgentTest {
 
-    public static void main(String[] args) {
-        GameModel model = new GameModel(10000);
-        GameState state = model.new GameState(new int[][]{
-            {0, 2, 0, 2},
-            {2, 4, 8, 4},
-            {4, 8, 16, 8},
-            {1024, 256, 128, 2},
-        });
-        MctsAgent gpa = new MctsAgent();
-        GameAction[] actionList = GameAction.values();
-        int[] counter = new int[actionList.length];
-        double[] percentage = new double[actionList.length];
-        for(int i = 0; i<1000; i++){
-            GameAction chosenAction = gpa.selectAction(state, model);
-            counter[chosenAction.id]++;
-        }
-        
-        for(int i = 0; i<actionList.length; i++){
-            System.out.println(actionList[i] + ": " + counter[i] + "(" + (counter[i] / 1000.0) + "%)");
-        }
-    }
-    
     public void testSelect() {
-        GameModel model = new GameModel(1000);
-        GameState state = model.new GameState(new int[][]{
-            {0, 2, 0, 0},
-            {2, 4, 0, 32},
-            {64, 16, 128, 2},
-            {512, 256, 0, 128},
-        });
-        MctsAgent gpa = new MctsAgent();
-        GameAction chosenAction = gpa.selectAction(state, model);
-        System.out.println(chosenAction);
+        
     }
 
     @Test
