0b5963500d3ba21e382d0fa2d7ab0efe7f426e49
Jiang Han <jianghan99@gmail.com>
2022-04-23 22:02:41 +0700

    Add idOfMin, idOfMax, average method is StatHelper

--- src/util/StatHelper.java
+++ src/util/StatHelper.java
@@ -1,10 +1,38 @@
 package util;
 
+import java.util.Arrays;
+
 /**
  * Helper class for statistic calculations
  * 
  * @author Jiang Han
  */
 public class StatHelper {
+    public static int idOfMinimum(int[] data) {
+        int minId = 0;
+        
+        for(int i = 1; i<data.length; i++){
+            if (data[minId] > data[i]){
+                minId = i;
+            }
+        }
+        
+        return minId;
+    }
     
+    public static int idOfMaximum(int[] data) {
+        int maxId = 0;
+        
+        for(int i = 1; i<data.length; i++){
+            if (data[maxId] < data[i]){
+                maxId = i;
+            }
+        }
+        
+        return maxId;
+    }
+    
+    public static double average(int[] data){
+        return Arrays.stream(data).average().getAsDouble();
+    }
 }
