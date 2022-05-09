646ee1cd62920c3496c82aad66ac428bf72816b2
Jiang Han <jianghan99@gmail.com>
2022-05-09 21:30:11 +0700

    Add call to ping LINE

---
+++ src/util/ExperimentDirector.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import controller.ExperimentController;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jiang Han
+ */
+public class ExperimentDirector {
+    public static void main(String[] args) {
+        testVPlayoutValues();
+    }
+    private static void testVPlayoutValues(){
+        
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter
+                .ofPattern("yyyyMMdd_HHmmss_SSS")));
+        
+        double[] vInits = {0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4};
+        int[] maxIters = {1000, 10000, 100000};
+        
+        for(int i = 0; i<maxIters.length; i++){
+            for (int j = 0; j < vInits.length; j++) {
+                ExperimentController.tdtsAverage(100, maxIters[i], Math.sqrt(2), 1, 1, true, true, i);
+            }
+        }
+        try {
+            HttpHelper.pingMe();
+        } catch (IOException ex) {
+            Logger.getLogger(ExperimentDirector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
