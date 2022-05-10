1e23f63276ddac00825093a4d22256a36d5d43a7
Jiang Han <47552445+jghjianghan@users.noreply.github.com>
2022-05-10 16:43:59 +0700

    Create ExperimentDirector.java

---
+++ src/util/ExperimentDirector.java
@@ -0,0 +1,49 @@
+/*
+ * Kelas tambahan untuk melakukan mengotomasi batch experiment. 
+ */
+package util;
+
+import controller.ExperimentController;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jiang Han
+ */
+public class ExperimentDirector {
+    public static void main(String[] args) {
+        testVPlayoutValuesPatch();
+    }
+    private static void testNormMethods(){
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter
+                .ofPattern("yyyyMMdd_HHmmss_SSS")));
+        
+        boolean[] isSpaceLocal = {false, true};
+        int[] maxIters = {1000, 10000, 100000};
+        
+        //MCTS
+        for(int i = 0; i<maxIters.length; i++){
+            for (int j = 0; j < isSpaceLocal.length; j++) {
+                ExperimentController.mctsAverage(100, maxIters[i], Math.sqrt(2), true, isSpaceLocal[j]);
+            }
+        }
+      
+        //TDTS
+        for(int i = 0; i<maxIters.length; i++){
+            for (int j = 0; j < isSpaceLocal.length; j++) {
+                ExperimentController.tdtsAverage(100, maxIters[i], Math.sqrt(2), 1, 1, true, isSpaceLocal[j]);
+            }
+        }
+      
+      
+        try {
+            HttpHelper.pingMe();
+        } catch (IOException ex) {
+            Logger.getLogger(ExperimentDirector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
