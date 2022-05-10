7ec52997e93bab5fdfc6b38594119c096f8636af
Jiang Han <jianghan99@gmail.com>
2022-05-10 16:45:54 +0700

    Test larger values of v_playouts

--- src/util/ExperimentDirector.java
+++ src/util/ExperimentDirector.java
@@ -18,19 +18,37 @@ import java.util.logging.Logger;
  */
 public class ExperimentDirector {
     public static void main(String[] args) {
-        testVPlayoutValues();
+        testVPlayoutValuesPatch();
     }
     private static void testVPlayoutValues(){
-        
         System.out.println(LocalDateTime.now().format(DateTimeFormatter
                 .ofPattern("yyyyMMdd_HHmmss_SSS")));
         
-        double[] vInits = {0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4};
+        double[] vInits = {0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 8, 16, 32, 64, 128};
         int[] maxIters = {1000, 10000, 100000};
         
         for(int i = 0; i<maxIters.length; i++){
             for (int j = 0; j < vInits.length; j++) {
-                ExperimentController.tdtsAverage(100, maxIters[i], Math.sqrt(2), 1, 1, true, true, i);
+                ExperimentController.tdtsAverage(100, maxIters[i], Math.sqrt(2), 1, 1, true, true, vInits[j]);
+            }
+        }
+        try {
+            HttpHelper.pingMe();
+        } catch (IOException ex) {
+            Logger.getLogger(ExperimentDirector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private static void testVPlayoutValuesPatch(){
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter
+                .ofPattern("yyyyMMdd_HHmmss_SSS")));
+        
+        double[] vInits = {8, 16, 32, 64, 128};
+        int[] maxIters = {1000, 10000, 100000};
+        
+        for(int i = 0; i<maxIters.length; i++){
+            for (int j = 0; j < vInits.length; j++) {                
+                ExperimentController.tdtsAverage(100, maxIters[i], Math.sqrt(2), 1, 1, true, true, vInits[j]);
             }
         }
         try {
